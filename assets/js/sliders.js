c71958b9c6798d4616fc375ae4da941cec3a974f
the_archi <direct@aloe-group.ru>
2023-02-13 14:02:27 +0700

    upd: sliders

---
+++ assets/js/sliders.js
@@ -0,0 +1,52 @@
+var swiper = new Swiper(".banner-slider", {
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+
+if (window.innerWidth > 767) {
+    var swiper = new Swiper(".product-slider", {
+        cssMode: true,
+        scrollbar: {
+            el: ".swiper-scrollbar",
+            hide: true, 
+        },
+        slidesPerView: 4,
+        spaceBetween: 20,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+  } else {
+    var swiper = new Swiper(".product-slider", {
+        cssMode: true,
+        scrollbar: {
+            el: ".swiper-scrollbar",
+            hide: true,
+        },
+        slidesPerView: 1,
+        spaceBetween: 20,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+  }
