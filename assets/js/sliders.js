112c12ba68dcc191ec4e0e698b62be4131c9fd5b
Kirill Verzhitskiy <verzhitskiy_kg@sgmk-group.ru>
2023-02-27 16:53:09 +0700

    fix: add progress slider

--- assets/js/sliders.js
+++ assets/js/sliders.js
@@ -1,45 +1,66 @@
-var mySwiper = new Swiper(".banner-slider", {
-    cssMode: true,
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-      },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    }
+const delay = 5000;
+
+const bannerSlider = new Swiper('.banner-slider', {
+  cssMode: true,
+  autoplay: {
+    delay,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+bannerSlider.on('slideChange', function () {
+  window._interval = 0;
 });
 
 if (window.innerWidth > 767) {
-    var swiper = new Swiper(".product-slider", {
-        cssMode: true,
-        scrollbar: {
-            el: ".swiper-scrollbar",
-            hide: false, 
-        },
-        slidesPerView: 4,
-        spaceBetween: 20,
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-    });
-  } else if (window.innerWidth < 767) {
-    var swiper = new Swiper(".product-slider", {
-        cssMode: true,
-        scrollbar: {
-            el: ".swiper-scrollbar",
-            hide: false,
-        },
-        slidesPerView: 1,
-        spaceBetween: 20,
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-    });
-  }
+  new Swiper('.product-slider', {
+    cssMode: true,
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      hide: false,
+    },
+    slidesPerView: 4,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+} else if (window.innerWidth < 767) {
+  new Swiper('.product-slider', {
+    cssMode: true,
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      hide: false,
+    },
+    slidesPerView: 1,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+}
+
+const setProgress = (value) => {
+  document.documentElement.style.setProperty('--slider-progress', `${value}deg`);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  setProgress(0);
+  const intervalSpeed = 10;
+  window._interval = 0;
+  setInterval(() => {
+    window._interval += intervalSpeed;
+    const percent = window._interval / delay * 100;
+    setProgress(percent / 100 * 180);
+  }, intervalSpeed);
+});
